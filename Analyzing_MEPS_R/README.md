93d0ff9a1e0548f2c9f54e9fa00b27dced13d78d
emitch8 <mitcheem@gmail.com>
2016-12-07 17:21:51 -0500

    fixing broken link, again

---
+++ Analyzing_MEPS_R/README.md
@@ -0,0 +1,9 @@
+# Analyzing MEPS data using R
+
+This repository is intended to provide example R code for those interested in analyzing data from the Agency for Healthcare Research and Quality's (AHRQ) Medical Expenditure Panel Survey (MEPS).
+
+[loading_MEPS.R](loading_MEPS.R) provides example code to download MEPS files from <a href = "https://meps.ahrq.gov/mepsweb/data_stats/download_data_files.jsp" target = "_blank">the MEPS website</a>
+
+Additional examples of analyzing MEPS data are coming soon!
+
+Please email me at emily.mitchell@ahrq.hhs.gov if you find any bugs.
---
+++ Analyzing_MEPS_R/loading_MEPS.R
@@ -0,0 +1,77 @@
+##########################################################################
+#
+# R programming statements to read in MEPS Public Use Files
+#
+# Updated: 11/18/2016
+#
+# This file contains example programming statements in R for users who 
+# want to read in a MEPS public use file (PUF) and save the dataset
+# as an R file for easier access.  
+#
+# This code uses the 'foreign' package in R to read the SAS transport 
+# file (.xpt) that is provided with each PUF release. Two methods of 
+# loading this data are given. The first requires the user to navigate to
+# the website containing the MEPS dataset and manually download the SAS
+# transport file using either the .zip or .exe files. The second method
+# uses the R function 'download.file' to automatically download the file 
+# by pointing to its location on the website. 
+#
+# Any statistical analyses on MEPS data should be conducted using the 
+# 'survey' package in R, in order to calculate appropriate standard errors 
+# for estimates from a survey with a complex sample design. Example R code 
+# for analyzing MEPS data using the 'survey' package can be found at:
+# https://meps.ahrq.gov/survey_comp/hc_samplecodes_se.shtml
+#
+# Users should be warned that the resulting R dataset does not preserve 
+# any SAS formats or variable labels. In addition, variable types may be 
+# lost. For instance, a character variable on the SAS dataset could be 
+# read as a factor variable in the R dataset. Users are encouraged to be 
+# diligent in confirming that variables are stored as the appropriate 
+# type before proceeding with analyses. 
+#
+# Please contact 
+#   Anita Soni at Anita.Soni@ahrq.hhs.gov or 
+#   Emily Mitchell at emily.mitchell@ahrq.hhs.gov for any questions
+#
+##########################################################################  
+
+# Install and load 'foreign' package to read SAS transport files
+    
+    install.packages("foreign")  
+    library(foreign)
+    
+##########################################################################   
+    
+# Option 1.  Read file from local computer
+    
+#    Step 1: Download and extract the SAS transport format zip file into "C:\MEPS\SASDATA"
+#    Step 2: Use 'read.xport' function to load file
+
+# Example: 2014 Full-Year Consolidated Data File (h171)
+    
+    meps_data = read.xport("C:/MEPS/SASDATA/h171.ssp")
+    
+##########################################################################  
+    
+# Option 2. Read file directly from website
+    
+#    Step 1: Use 'download.file' function to save zip file from MEPS website to temporary file 'temp'
+#    Step 2: Use 'read.xport' and 'unzip' to unzip and load SAS transport data
+    
+# Example: 2014 Full-Year Consolidated Data File (h171)
+    
+   filename = "h171"    
+   download.file(sprintf("https://meps.ahrq.gov/mepsweb/data_files/pufs/%sssp.zip",filename),temp <- tempfile())   
+   meps_data = read.xport(unzip(temp))   
+   unlink(temp)  ## Remove the temp file to free up memory
+   
+##########################################################################
+
+# Optional: Save data as .RData file for faster loading
+   
+   save(meps_data,file = "C:/MEPS/SASDATA/h171.RData")
+   
+# Once the .Rdata is saved, it can be re-loaded in a new R session using the following code:
+   
+   load(file = "C:/MEPS/SASDATA/h171.RData")
+   
