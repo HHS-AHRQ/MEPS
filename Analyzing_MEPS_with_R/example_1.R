5895ebbef7635d8627dddd8149d77dad5dc5b5d7
Emily Mitchell <mitcheem@gmail.com>
2017-02-03 14:56:49 -0500

    removing output

--- Analyzing_MEPS_with_R/example_1.R
+++ Analyzing_MEPS_with_R/example_1.R
@@ -11,19 +11,14 @@ mepsdsgn <- svydesign(id = ~VARPSU,
 # Since standard errors are not applicable to population control totals, we don't need to use a survey function here.
 # Instead, the total population is just equal to the sum of survey weights (PERWT13F).
 sum(FYC2013$PERWT13F) 
-##  [1] 315721982
 
 # TOTAL EXPENSES
 # Here, we use the 'svytotal' function with the '$' notation to indicate that we want to use dataset FYC2013 and variable TOTEXP13.
 svytotal(FYC2013$TOTEXP13, design = mepsdsgn)  
-##           total         SE
-## [1,] 1.4005e+12 4.3378e+10
 
 # Alterantively, we can use the formula notation '~', to tell R that we want to look in the design object `mepsdsgn` 
 # (in which we defined dataset FYC2013) to find the variable TOTEXP13.
 svytotal(~TOTEXP13,design = mepsdsgn)  
-##               total         SE
-## TOTEXP13 1.4005e+12 4.3378e+10
 
 # PERCENT WITH EXPENSE
 # To calculate the percent of people with any expense, first create a new indicator variable for persons with an expense:
@@ -38,49 +33,23 @@ mepsdsgn <- svydesign(id = ~VARPSU,
     data = FYC2013, 
     nest=TRUE)
 svymean(~any_expense,design = mepsdsgn)                      
-##                mean     SE
-## any_expense 0.84398 0.0036
 
 # MEAN AND MEDIAN EXPENSE, PER PERSON
 # To get expenses per person with an expense, we limit the dataset to persons that have an expense (i.e. any_expense == 1), 
 # using the 'subset' function.
 svymean(~TOTEXP13, design = subset(mepsdsgn,any_expense==1)) 
-##          mean     SE
-## TOTEXP13 5256 118.17
 svyquantile(~TOTEXP13, design = subset(mepsdsgn,any_expense==1),quantiles = 0.5) 
-##           0.5
-## TOTEXP13 1389
+
 
 # DISTIBRUTION BY SOURCE OF PAYMENT
 # For percent of total, we need to use the `svyratio` function, and specify the numerator and denominator. 
 # First, estimate the percent for out-of-pocket payments (`TOTSLF13`).
 svyratio(~TOTSLF13, denominator = ~TOTEXP13, design = mepsdsgn)
-## Ratio estimator: svyratio.survey.design2(~TOTSLF13, denominator = ~TOTEXP13, design = mepsdsgn)
-## Ratios=
-##           TOTEXP13
-## TOTSLF13 0.1377617
-## SEs=
-##             TOTEXP13
-## TOTSLF13 0.004395095
 
 # We can calculate percentages for multiple variables at one time, using a '+' sign in the formula notation.
 svyratio(~TOTSLF13 + TOTPTR13 + TOTMCR13 + TOTMCD13, 
          denominator = ~TOTEXP13, 
          design = mepsdsgn)
-## Ratio estimator: svyratio.survey.design2(~TOTSLF13 + TOTPTR13 + TOTMCR13 + TOTMCD13, 
-##     denominator = ~TOTEXP13, design = mepsdsgn)
-## Ratios=
-##           TOTEXP13
-## TOTSLF13 0.1377617
-## TOTPTR13 0.4060437
-## TOTMCR13 0.2530722
-## TOTMCD13 0.1243487
-## SEs=
-##             TOTEXP13
-## TOTSLF13 0.004395095
-## TOTPTR13 0.011466596
-## TOTMCR13 0.009134258
-## TOTMCD13 0.007487569
 
 # Before estimating percentages for 'Other' insurance, we need to adjust this variable to match the online table: 
 # Other = VA + worker's comp + other sources. 
@@ -89,10 +58,3 @@ svyratio(~TOTSLF13 + TOTPTR13 + TOTMCR13 + TOTMCD13,
 mepsdsgn <- update(mepsdsgn, tototh13 = TOTVA13 + TOTWCP13 + TOTOTH13)
 svyratio(~tototh13, denominator = ~TOTEXP13, design = mepsdsgn)
 
-## Ratio estimator: svyratio.survey.design2(~tototh13, denominator = ~TOTEXP13, design = mepsdsgn)
-## Ratios=
-##            TOTEXP13
-## tototh13 0.07877691
-## SEs=
-##             TOTEXP13
-## tototh13 0.005148356
