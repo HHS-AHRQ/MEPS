3259fc4000e3826a3f3cd4cf6f247c680bb04653
Emily Mitchell <mitcheem@gmail.com>
2017-02-10 10:42:30 -0500

    Streamlining to useful code only

--- Analyzing_MEPS_with_R/example_1.R
+++ Analyzing_MEPS_with_R/example_1.R
@@ -5,56 +5,39 @@ mepsdsgn <- svydesign(id = ~VARPSU,
     strata = ~VARSTR, 
     weights = ~PERWT13F, 
     data = FYC2013, 
-    nest=TRUE)  
+    nest = TRUE)  
 
 # TOTAL POPULATION
-# Since standard errors are not applicable to population control totals, we don't need to use a survey function here.
-# Instead, the total population is just equal to the sum of survey weights (PERWT13F).
+# Standard errors are not applicable to population control totals, so we don't need to use a survey function here.
+# The total population is equal to the sum of survey weights (PERWT13F).
 sum(FYC2013$PERWT13F) 
 
 # TOTAL EXPENSES
-# Here, we use the 'svytotal' function with the '$' notation to indicate that we want to use dataset FYC2013 and variable TOTEXP13.
-svytotal(FYC2013$TOTEXP13, design = mepsdsgn)  
-
-# Alterantively, we can use the formula notation '~', to tell R that we want to look in the design object `mepsdsgn` 
-# (in which we defined dataset FYC2013) to find the variable TOTEXP13.
+# Use the formula notation '~' with specified design object for survey functions
 svytotal(~TOTEXP13,design = mepsdsgn)  
 
 # PERCENT WITH EXPENSE
-# To calculate the percent of people with any expense, first create a new indicator variable for persons with an expense:
-FYC2013$any_expense = (FYC2013$TOTEXP13 > 0)*1
+# To calculate the percent of people with any expense, first update mepsdsgn with a new indicator variable for persons with an expense:
+mepsdsgn <- update(mepsdsgn, any_expense = (TOTEXP13 > 0)*1)
 
-# Note: If we try to run 'svymean' now, we will get an error, since we added a variable to the data set after defining the survey design
-# object, 'mepsdsgn'. First, re-run the code defining `mepsdsgn` to include the dataset with the new variable. Then run the 'svymean'
-# function, since the `mepsdsgn` object now includes the version of the dataset that contains the new variable.
-mepsdsgn <- svydesign(id = ~VARPSU, 
-    strata = ~VARSTR, 
-    weights = ~PERWT13F, 
-    data = FYC2013, 
-    nest=TRUE)
+# Then run the 'svymean' function
 svymean(~any_expense,design = mepsdsgn)                      
 
 # MEAN AND MEDIAN EXPENSE, PER PERSON
-# To get expenses per person with an expense, we limit the dataset to persons that have an expense (i.e. any_expense == 1), 
-# using the 'subset' function.
+# To get expenses per person with an expense, use the 'subset' function to limit the dataset to persons that have an expense 
+# (i.e. any_expense == 1).
 svymean(~TOTEXP13, design = subset(mepsdsgn,any_expense==1)) 
 svyquantile(~TOTEXP13, design = subset(mepsdsgn,any_expense==1),quantiles = 0.5) 
 
-
-# DISTIBRUTION BY SOURCE OF PAYMENT
-# For percent of total, we need to use the `svyratio` function, and specify the numerator and denominator. 
-# First, estimate the percent for out-of-pocket payments (`TOTSLF13`).
-svyratio(~TOTSLF13, denominator = ~TOTEXP13, design = mepsdsgn)
-
-# We can calculate percentages for multiple variables at one time, using a '+' sign in the formula notation.
+# DISTRIBUTION BY SOURCE OF PAYMENT
+# For percent of total, use the `svyratio` function, and specify the numerator and denominator. 
+# Use a '+' sign to calculate estimates for multiple variables. 
 svyratio(~TOTSLF13 + TOTPTR13 + TOTMCR13 + TOTMCD13, 
          denominator = ~TOTEXP13, 
          design = mepsdsgn)
 
 # Before estimating percentages for 'Other' insurance, we need to adjust this variable to match the online table: 
 # Other = VA + worker's comp + other sources. 
-# Previously, we did this by adding new variables to the dataset FYC2013, and then re-defining the design object 'mepsdsgn'. 
-# But we can streamline this process by using the 'update' function to make changes to 'mepsdsgn' directly.
 mepsdsgn <- update(mepsdsgn, tototh13 = TOTVA13 + TOTWCP13 + TOTOTH13)
 svyratio(~tototh13, denominator = ~TOTEXP13, design = mepsdsgn)
 
