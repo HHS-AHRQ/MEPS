199a212e1c65fa4055631bb23ab1f09c6c816a31
Mitchell <Emily.Mitchell@ahrq.hhs.gov>
2017-04-03 10:25:00 -0400

    Removing references to loading_meps code

--- R/example_1.R
+++ R/example_1.R
@@ -1,43 +1,42 @@
 # Example 1: Re-producing estimates from MEPS summary table for 2013 data
 
-# After downloading MEPS data (see 'loading_MEPS.R' for instructions to do so) define the survey object:
-mepsdsgn <- svydesign(id = ~VARPSU, 
-    strata = ~VARSTR, 
-    weights = ~PERWT13F, 
-    data = FYC2013, 
-    nest = TRUE)  
+# After downloading MEPS data define the survey object:
+mepsdsgn <- svydesign(id = ~VARPSU,
+    strata = ~VARSTR,
+    weights = ~PERWT13F,
+    data = FYC2013,
+    nest = TRUE)
 
 # TOTAL POPULATION
 # Standard errors are not applicable to population control totals, so we don't need to use a survey function here.
 # The total population is equal to the sum of survey weights (PERWT13F).
-sum(FYC2013$PERWT13F) 
+sum(FYC2013$PERWT13F)
 
 # TOTAL EXPENSES
 # Use the formula notation '~' with specified design object for survey functions
-svytotal(~TOTEXP13,design = mepsdsgn)  
+svytotal(~TOTEXP13,design = mepsdsgn)
 
 # PERCENT WITH EXPENSE
 # To calculate the percent of people with any expense, first update mepsdsgn with a new indicator variable for persons with an expense:
 mepsdsgn <- update(mepsdsgn, any_expense = (TOTEXP13 > 0)*1)
 
 # Then run the 'svymean' function
-svymean(~any_expense,design = mepsdsgn)                      
+svymean(~any_expense,design = mepsdsgn)
 
 # MEAN AND MEDIAN EXPENSE, PER PERSON
-# To get expenses per person with an expense, use the 'subset' function to limit the dataset to persons that have an expense 
+# To get expenses per person with an expense, use the 'subset' function to limit the dataset to persons that have an expense
 # (i.e. any_expense == 1).
-svymean(~TOTEXP13, design = subset(mepsdsgn,any_expense==1)) 
-svyquantile(~TOTEXP13, design = subset(mepsdsgn,any_expense==1),quantiles = 0.5) 
+svymean(~TOTEXP13, design = subset(mepsdsgn,any_expense==1))
+svyquantile(~TOTEXP13, design = subset(mepsdsgn,any_expense==1),quantiles = 0.5)
 
 # DISTRIBUTION BY SOURCE OF PAYMENT
-# For percent of total, use the `svyratio` function, and specify the numerator and denominator. 
-# Use a '+' sign to calculate estimates for multiple variables. 
-svyratio(~TOTSLF13 + TOTPTR13 + TOTMCR13 + TOTMCD13, 
-         denominator = ~TOTEXP13, 
+# For percent of total, use the `svyratio` function, and specify the numerator and denominator.
+# Use a '+' sign to calculate estimates for multiple variables.
+svyratio(~TOTSLF13 + TOTPTR13 + TOTMCR13 + TOTMCD13,
+         denominator = ~TOTEXP13,
          design = mepsdsgn)
 
-# Before estimating percentages for 'Other' insurance, we need to adjust this variable to match the online table: 
-# Other = VA + worker's comp + other sources. 
+# Before estimating percentages for 'Other' insurance, we need to adjust this variable to match the online table:
+# Other = VA + worker's comp + other sources.
 mepsdsgn <- update(mepsdsgn, tototh13 = TOTVA13 + TOTWCP13 + TOTOTH13)
 svyratio(~tototh13, denominator = ~TOTEXP13, design = mepsdsgn)
-
--- R/example_2.R
+++ R/example_2.R
@@ -1,29 +1,29 @@
 # Example 2: Re-producing estimates for Figure 1 of Stat brief #491
 
-# After downloading MEPS data (see 'loading_MEPS.R' for instructions to do so) define the survey object:
-mepsdsgn <- svydesign(id = ~VARPSU, 
-    strata = ~VARSTR, 
-    weights = ~PERWT13F, 
-    data = FYC2013, 
-    nest = TRUE)  
+# After downloading MEPS data define the survey object:
+mepsdsgn <- svydesign(id = ~VARPSU,
+    strata = ~VARSTR,
+    weights = ~PERWT13F,
+    data = FYC2013,
+    nest = TRUE)
 
 # To get ambulatory (OB+OP) and home health/other expenditures, add variables to the mepsdsgn object.
-mepsdsgn <- update(mepsdsgn, 
+mepsdsgn <- update(mepsdsgn,
                    ambexp13 = OBVEXP13 + OPTEXP13 + ERTEXP13,
                    hhexp13  = HHAEXP13 + HHNEXP13 + VISEXP13 + OTHEXP13)
 
-# Use svyratio to calculate percentage distribution of spending by type of service: 
-pct_TOS = svyratio(~IPTEXP13 + ambexp13 + RXEXP13 + DVTEXP13 + hhexp13, 
-                   denominator = ~TOTEXP13, 
+# Use svyratio to calculate percentage distribution of spending by type of service:
+pct_TOS = svyratio(~IPTEXP13 + ambexp13 + RXEXP13 + DVTEXP13 + hhexp13,
+                   denominator = ~TOTEXP13,
                    design = mepsdsgn)
 
 # Now do the same thing by age group (<65, 65+), using the `subset` function.
-pct_TOS_lt65 = svyratio(~IPTEXP13 + ambexp13 + RXEXP13 + DVTEXP13 + hhexp13, 
-                    denominator = ~TOTEXP13, 
+pct_TOS_lt65 = svyratio(~IPTEXP13 + ambexp13 + RXEXP13 + DVTEXP13 + hhexp13,
+                    denominator = ~TOTEXP13,
                     design = subset(mepsdsgn,AGELAST < 65))
 
-pct_TOS_ge65 = svyratio(~IPTEXP13 + ambexp13 + RXEXP13 + DVTEXP13 + hhexp13, 
-                    denominator = ~TOTEXP13, 
+pct_TOS_ge65 = svyratio(~IPTEXP13 + ambexp13 + RXEXP13 + DVTEXP13 + hhexp13,
+                    denominator = ~TOTEXP13,
                     design = subset(mepsdsgn,AGELAST >= 65))
 
 # Create output tables
@@ -43,7 +43,7 @@ write.csv(pct_matrix,file = "C:/MEPS/figure1.csv")
 
 ## Graphics - barplot Example to recreate Figure 1
 bp <- barplot(
-  t(pct_matrix),        # 't' transposes the matrix, so the x-axis represents type of service 
+  t(pct_matrix),        # 't' transposes the matrix, so the x-axis represents type of service
   beside=TRUE,          # make bars side-by-side, not stacked
   col   = c("blue","yellow","magenta"),   # change colors of bars
   ylab  = "Percentage", # change y-axis label
@@ -54,7 +54,7 @@ text(x = bp, y = t(pct_matrix)+2,  # add text labels to end points of bars
      xpd=T,col="blue",font=2)
 
 
-## Graphics example with ggplot2: 
+## Graphics example with ggplot2:
 install.packages("reshape2")
 install.packages("ggplot2")
 
@@ -70,9 +70,9 @@ my_magenta <- rgb(99,16,99,maxColorValue=255)
 my_darkblue <- rgb(0,0,173,maxColorValue = 255)
 
 # create plot
-ggplot(long,aes(x=Var1,y=value,fill=Var2)) +    
-  geom_bar(position = "dodge",stat="identity") +                 # make bars side-by-side 
-  scale_fill_manual(values = c(my_blue,my_yellow,my_magenta))+   # change colors of bars  
+ggplot(long,aes(x=Var1,y=value,fill=Var2)) +
+  geom_bar(position = "dodge",stat="identity") +                 # make bars side-by-side
+  scale_fill_manual(values = c(my_blue,my_yellow,my_magenta))+   # change colors of bars
   labs(y = "Percentage",x="") +                                  # change axis labels
   geom_text(aes(x=Var1,y=value,label=round(value)),              # add data labels to end of bars
             position = position_dodge(width = 0.9),vjust = -0.25,
