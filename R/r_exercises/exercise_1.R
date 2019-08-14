1c15fed4220e44a082216b48646f7583e55ffdee
Emily Mitchell <Emily.Mitchell@ahrq.hhs.gov>
2019-08-14 13:12:13 -0400

    Editing R codes

--- R/r_exercises/exercise_1.R
+++ R/r_exercises/exercise_1.R
@@ -1,45 +1,43 @@
 # -----------------------------------------------------------------------------
-# 
-# DESCRIPTION: THIS PROGRAM GENERATES THE FOLLOWING ESTIMATES ON 
-#               NATIONAL HEALTH CARE, 2016: 
-# 
-#             (1) OVERALL EXPENSES 
+# DESCRIPTION: THIS PROGRAM GENERATES THE FOLLOWING ESTIMATES ON
+#               NATIONAL HEALTH CARE, 2016:
+#
+#             (1) OVERALL EXPENSES
 # 	          (2) PERCENTAGE OF PERSONS WITH AN EXPENSE
 # 	          (3) MEAN EXPENSE PER PERSON WITH AN EXPENSE
-# 
-# 
-# INPUT FILE:  C:\MEPS\STATA\DATA\H192.ssp (2016 FULL-YEAR FILE)
+#
+# INPUT FILE:  C:/MEPS/H192.ssp (2016 FULL-YEAR FILE)
 # -----------------------------------------------------------------------------
 
 # Install and load libraries
 
-  # Can skip this part if already installed 
+  # Can skip this part if already installed
   install.packages("survey")
   install.packages("foreign")
   install.packages("dplyr")
 
-  # Run this part each time you re-start R  
+  # Run this part each time you re-start R
   library(survey)
   library(foreign)
   library(dplyr)
-  
-# Set options to deal with lonely psu 
+
+# Set options to deal with lonely psu
   options(survey.lonely.psu='adjust');
 
 
-# Read in data from 2016 consolidated data file (hc-192) 
+# Read in data from 2016 consolidated data file (hc-192)
   h192 = read.xport("C:/MEPS/h192.ssp")
 
-# Add variables for persons with any expense and persons under 65  
+# Add variables for persons with any expense and persons under 65
 
   h192 = h192 %>%
     mutate(
       has_exp = (TOTEXP16 > 0), # persons with any expense
       age_cat = ifelse(AGELAST < 65, "<65", "65+")  # persons under age 65
     )
-  
+
 # QC check on new variables
-  
+
   h192 %>%
     group_by(has_exp) %>%
     summarise(min = min(TOTEXP16), max = max(TOTEXP16))
@@ -47,30 +45,26 @@
   h192 %>%
     group_by(age_cat) %>%
     summarise(min = min(AGELAST), max = max(AGELAST))
-  
-  
-# Define the survey design  
-  
-  mepsdsgn = svydesign(
-    id = ~VARPSU, 
-    strata = ~VARSTR, 
-    weights = ~PERWT16F, 
-    data = h192, 
-    nest = TRUE)  
 
-# Overall expenses 
+
+# Define the survey design
+
+  mepsdsgn = svydesign(
+    id = ~VARPSU,
+    strata = ~VARSTR,
+    weights = ~PERWT16F,
+    data = h192,
+    nest = TRUE)
+
+# Overall expenses
 svymean(~TOTEXP16, design = mepsdsgn)
-svytotal(~TOTEXP16, design = mepsdsgn)  
-  
+svytotal(~TOTEXP16, design = mepsdsgn)
+
 # Percentage of persons with an expense
 svymean(~has_exp, design = mepsdsgn)
 
 # Mean expense per person with an expense
 svymean(~TOTEXP16, design = subset(mepsdsgn, has_exp))
 
-
 # Mean expense per person with an expense, by age category
-svyby(~TOTEXP16, 
-      by = ~age_cat,
-      FUN = svymean, 
-      design = subset(mepsdsgn, has_exp))
+svyby(~TOTEXP16, by = ~age_cat, FUN = svymean, design = subset(mepsdsgn, has_exp))
--- R/r_exercises/exercise_3.R
+++ R/r_exercises/exercise_3.R
@@ -1,38 +1,38 @@
 # -----------------------------------------------------------------------------
-# DESCRIPTION:  THIS PROGRAM ILLUSTRATES HOW TO POOL MEPS DATA FILES FROM 
-#  DIFFERENT YEARS THE EXAMPLE USED IS POPULATION AGE 26-30 WHO ARE UNINSURED 
+# DESCRIPTION:  THIS PROGRAM ILLUSTRATES HOW TO POOL MEPS DATA FILES FROM
+#  DIFFERENT YEARS THE EXAMPLE USED IS POPULATION AGE 26-30 WHO ARE UNINSURED
 #  BUT HAVE HIGH INCOME DATA FROM 2015 AND 2016 ARE POOLED.
-# 
+#
 # VARIABLES WITH YEAR-SPECIFIC NAMES MUST BE RENAMED BEFORE COMBINING FILES:
 #
-# IN THIS PROGRAM THE INSURANCE COVERAGE VARIABLES 'INSCOV15' AND 'INSCOV16' 
+# IN THIS PROGRAM THE INSURANCE COVERAGE VARIABLES 'INSCOV15' AND 'INSCOV16'
 #  ARE RENAMED TO 'INSCOV'.
-# 
+#
 # SEE HC-036 (1996-2015 POOLED ESTIMATION FILE) FOR
 #  INSTRUCTIONS ON POOOLING AND CONSIDERATIONS FOR VARIANCE
 #  ESTIMATION FOR PRE-2002 DATA.
-# 
-# INPUT FILES: (1) C:\MEPS\STATA\DATA\H192.ssp (2016 FULL-YEAR FILE)
-# 	           (2) C:\MEPS\STATA\DATA\H181.ssp (2015 FULL-YEAR FILE)
+#
+# INPUT FILES: (1) C:/MEPS/H192.ssp (2016 FULL-YEAR FILE)
+# 	           (2) C:/MEPS/H181.ssp (2015 FULL-YEAR FILE)
 # -----------------------------------------------------------------------------
-  
+
 # Install and load libraries
-  
-  # Can skip this part if already installed 
+
+  # Can skip this part if already installed
   install.packages("survey")
   install.packages("foreign")
   install.packages("dplyr")
-  
-  # Run this part each time you re-start R  
+
+  # Run this part each time you re-start R
   library(survey)
   library(foreign)
   library(dplyr)
 
-# Set options to deal with lonely psu 
+# Set options to deal with lonely psu
 options(survey.lonely.psu='adjust');
 
 
-# Read in data 
+# Read in data
 h192 = read.xport("C:/MEPS/h192.ssp") # 2016 FYC
 h181 = read.xport("C:/MEPS/h181.ssp") # 2015 FYC
 
@@ -52,31 +52,30 @@ h181x = h181 %>%
   select(DUPERSID, VARSTR, VARPSU, AGELAST, inscov, perwt, povcat, totslf)
 
 # Stack data and define pooled weight variable and subpop of interest
-#  subpop = age 26-30, uninsured, high income 
+#  subpop = age 26-30, uninsured, high income
 
 pool = bind_rows(h192x, h181x) %>%
   mutate(poolwt = perwt / 2, # divide perwt by number of years (2)
-         subpop = (26 <= AGELAST & AGELAST <= 30 & povcat == 5 & inscov == 3)) 
+         subpop = (26 <= AGELAST & AGELAST <= 30 & povcat == 5 & inscov == 3))
 
 # QC subpop
 
-pool %>% 
+pool %>%
   filter(subpop) %>%
   group_by(AGELAST, povcat, inscov) %>%
   summarise(n())
 
 
-# Define the survey design  
+# Define the survey design
 
 mepsdsgn = svydesign(
-  id = ~VARPSU, 
-  strata = ~VARSTR, 
-  weights = ~poolwt, 
-  data = pool, 
-  nest = TRUE)  
+  id = ~VARPSU,
+  strata = ~VARSTR,
+  weights = ~poolwt,
+  data = pool,
+  nest = TRUE)
 
 
 # Weighted estimate on totslf for combined data w/age=26-30, uninsured whole year, and high income
 
 svymean(~totslf, design = subset(mepsdsgn, subpop))
-
